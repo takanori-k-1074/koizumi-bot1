d31b3707d0a783bdc74921da33794bf6bade5a6e
takanori-k-1074 <64514926+takanori-k-1074@users.noreply.github.com>
2020-07-02 20:11:26 +0900

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ require 'kconv'
 require 'rexml/document'
 
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
+  protect_from_forgery :except => [:callback]
 
   before_action :validate_signature
 
