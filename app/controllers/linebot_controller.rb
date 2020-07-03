7c7d9a375d5bd7b2a45ef984b867b0d73efc1165
takanori-k-1074 <64514926+takanori-k-1074@users.noreply.github.com>
2020-07-03 19:29:14 +0900

    update

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -51,41 +51,41 @@ class LinebotController < ApplicationController
             client.reply_message(event['replyToken'], message)
           elsif event.message['text'].include?("名前")
             name = {
-              "type": "bubble",
-              "header": {
-                "type": "box",
-                "layout": "vertical",
-                "contents": [
+              type: "bubble",
+              header: {
+                type: "box",
+                layout: "vertical",
+                contents: [
                   {
-                    "type": "text",
-                    "text": "hello, world",
-                    "decoration": "underline"
+                    type: "text",
+                    text: "hello, world",
+                    decoration: "underline"
                   }
                 ],
-                "margin": "none",
-                "spacing": "none",
-                "backgroundColor": "#ddffdd"
+                margin: "none",
+                spacing: "none",
+                backgroundColor: "#ddffdd"
               },
-              "hero": {
-                "type": "image",
-                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-                "size": "full",
-                "aspectMode": "cover"
+              hero: {
+                type: "image",
+                url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+                size: "full",
+                aspectMode: "cover"
               },
-              "body": {
-                "type": "box",
-                "layout": "vertical",
-                "contents": [
+              body: {
+                type: "box",
+                layout: "vertical",
+                contents: [
                   {
-                    "type": "text",
-                    "text": "コイズミBOT試作１号機",
-                    "align": "center",
-                    "weight": "bold",
-                    "size": "xl",
-                    "color": "#aa0000"
+                    type: "text",
+                    text: "コイズミBOT試作１号機",
+                    align: "center",
+                    weight: "bold",
+                    size: "xl",
+                    color: "#aa0000"
                   }
                 ],
-                "backgroundColor": "#ddffdd"
+                backgroundColor: "#ddffdd"
               }
             }
             client.reply_message(event['replyToken'], name)
